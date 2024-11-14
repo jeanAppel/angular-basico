78752d2da8364f7993c73c0d389138c184cd8692
jeanAppel <jean.appel@hotmail.com>
2024-11-14 18:19:48 -0300

    35 - Função de seleção

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
@@ -17,4 +19,44 @@ export class Componente11Component {
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
   });
 
+  // Visibilidade dos botões
+  btnCadastrar:boolean = true;
+
+  // Vetor
+  vetor:Pessoa[] = [];
+
+  // Armazenar índice da pessoa selecionada
+  indice:number = -1;
+
+  // Função de cadastro
+  cadastrar() {
+
+    // Cadastro no vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visualização via console
+    console.table(this.vetor);
+
+  }
+
+  // Função de seleção
+  selecionar(indice:number) {
+
+    // Atribuir o índice da pessoa
+    this.indice = indice;
+
+    // Atribuir os dados da pessoa no formulário
+    this.formulario.setValue({
+      nome: this.vetor[indice].nome,
+      idade: this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+
+    // Visibilidade dos botões
+    this.btnCadastrar = false;
+
+  }
 }
