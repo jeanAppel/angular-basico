bba04d83bad902a23ef184ac5111831c51a9804e
jeanAppel <jean.appel@hotmail.com>
2024-11-14 16:29:48 -0300

    29 - Mensagens de validação

---
+++ src/app/componente11/componente11.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-componente11',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './componente11.component.html',
+  styleUrl: './componente11.component.css'
+})
+export class Componente11Component {
+
+  // Objeto de formulário
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+
+}
